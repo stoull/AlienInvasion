42c49e435dc74e800078bb2e1a48be2c9fc79e89
stoull <changchunstep@gmail.com>
2020-11-09 21:27:42 +0800

    Create the window of the game

---
+++ main.py
@@ -0,0 +1,26 @@
+import sys
+import pygame
+
+class AlienInvasion:
+    """Overall class to manage game assets and behavior."""
+    def __init__(self):
+        """Initialize the game, and create game resources"""
+        pygame.init()
+        self.screen = pygame.display.set_mode((640, 400))
+        pygame.display.set_caption("Alien Invasion")
+        self.bg_color = (230, 230, 230)
+    def run_game(self):
+        """Start the main loop for the game"""
+        while True:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()
+
+            self.screen.fill(self.bg_color)
+            # Make the most recently drawn screen visible.
+            pygame.display.flip()
+
+
+if __name__ == '__main__':
+    ai = AlienInvasion()
+    ai.run_game()
