f5b9e79072fe40434a9d73d7c4e4d28b857be5cb
stoull <changchunstep@gmail.com>
2020-11-09 21:36:23 +0800

    refine settings file

---
+++ settings.py
@@ -0,0 +1,8 @@
+class Settings:
+    """A class to store all settings for Alien Invasion"""
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen settings
+        self.screen_width = 640
+        self.screen_height = 400
+        self.bg_color = (230, 230, 230)
